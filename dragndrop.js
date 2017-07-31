4afd3f1edbbf3d75c54e3d7c87c0034c09fa397b
madeleinejohanson <madeleine.johanson53@gmail.com>
2017-07-31 14:50:58 +1000

    v3.1

--- dragndrop.js
+++ dragndrop.js
@@ -111,31 +111,6 @@ var filterGroup = document.getElementById('filter-group');
           console.log(JSON.stringify(hiddenFilters))
           map.setFilter( 'fromdragndrop', hiddenFilters);
             
-
-        // When a click event occurs near a polygon, open a popup at the location of
-      // the feature, with description HTML from its properties.
-      map.on('click', function (e) {
-      var features = map.queryRenderedFeatures(e.point, { layers: [ "fromdragndrop"] });
-       if (!features.length) {
-        return;
-      }
-
-    var feature = features[0];
-    var feat = features.length;
-
-    var popup = new mapboxgl.Popup()
-        .setLngLat(map.unproject(e.point))
-        .setHTML([feature.properties.layer] + ["<br>"] + [feature.properties.tag])
-        .addTo(map);
-
-// Use the same approach as above to indicate that the symbols are clickable
-// by changing the cursor style to 'pointer'.
-map.on('mousemove', function (e) {
-    var features = map.queryRenderedFeatures(e.point, { layers: [ "fromdragndrop"] });
-    map.getCanvas().style.cursor = feat ? 'pointer' : '';
-});
-});
-
 });
         }
     
@@ -184,3 +159,28 @@ function handleDragOver(evt) {
 var dropZone = document.getElementById('drop_zone');
 dropZone.addEventListener('dragover', handleDragOver, false);
 dropZone.addEventListener('drop', handleFileSelect, false);
+
+
+        // When a click event occurs near a polygon, open a popup at the location of
+      // the feature, with description HTML from its properties.
+      map.on('click', function (e) {
+      var features = map.queryRenderedFeatures(e.point, { layers: [ "fromdragndrop"] });
+       if (!features.length) {
+        return;
+      }
+
+    var feature = features[0];
+    var feat = features.length;
+
+    var popup = new mapboxgl.Popup()
+        .setLngLat(map.unproject(e.point))
+        .setHTML([feature.properties.layer] + ["<br>"] + [feature.properties.tag])
+        .addTo(map);
+
+// Use the same approach as above to indicate that the symbols are clickable
+// by changing the cursor style to 'pointer'.
+map.on('mousemove', function (e) {
+    var features = map.queryRenderedFeatures(e.point, { layers: [ "fromdragndrop"] });
+    map.getCanvas().style.cursor = feat ? 'pointer' : '';
+});
+});
